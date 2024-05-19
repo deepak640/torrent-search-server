64d38922a264e218db0f1a758dfa89c39df3cc3f
deepak640 <ayushdeepnegi@gmail.com>
2024-05-19 12:03:18 +0530

    generated torrent file

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import TorrentSearchApi from "torrent-search-api";
 import WebTorrent from "webtorrent";
 import fs from "fs";
 import cliProgress from "cli-progress";
+import { magnetDecode } from "@ctrl/magnet-link";
+import ParseTorrent from "parse-torrent";
 
 const app = express();
 const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
@@ -20,43 +22,60 @@ const magnet = await TorrentSearchApi.getMagnet(magnetURI[0]);
 const torrentHtmlDetail = await TorrentSearchApi.getTorrentDetails(
   magnetURI[0]
 );
-// const buffer = await TorrentSearchApi.downloadTorrent(magnetURI[0]);
-// console.log("🚀 ~ buffer:", buffer)
 
-// Read the video file
 
-// client.add(magnetURI[0].magnet, (torrent) => {
-//   const files = torrent.files;
-//   let length = files.length;
-//   console.log("Number of files :- \t", +length);
-//   bar.start(100, 0);
-//   let interval = setInterval(() => {
-//     bar.update((torrent.progress * 100))
-//   }, 5000);
-//   files.forEach((file) => {
-//     const source = file.createReadStream();
-//     const destination = fs.createWriteStream(file.name);
-//     source
-//       .on("end", () => {
-//         console.log("file : \t\t", file.name);
-//         length -= 1;
-//         if (!length) {
-//           bar.stop()
-//           clearInterval(interval);
-//           process.exit();
-//         }
-//       })
-//       .pipe(destination);
-//   });
-// });
+// Function to convert magnet link to torrent file
+function magnetToTorrent(magnetURI, outputFilePath) {
+  const client = new WebTorrent();
 
-// const parsed = magnetDecode(magnets[0].magnet);
+  client.add(magnetURI, { announce: [] }, (torrent) => {
+    const torrentFileBuffer = torrent.torrentFile;
+
+    fs.writeFileSync(outputFilePath, torrentFileBuffer);
+    console.log(`Torrent file created at ${outputFilePath}`);
+
+    client.destroy(); // Destroy the client after the process is done
+  });
+
+  client.on('error', (err) => {
+    console.error(`Error: ${err.message}`);
+  });
+}
+
+
+// Example usage
+const outputFilePath = "output.torrent";
+magnetToTorrent(magnet, outputFilePath);
+
+const Download = () => {
+  client.add(magnetURI[0].magnet, (torrent) => {
+    const files = torrent.files;
+    let length = files.length;
+    console.log("Number of files :- \t", +length);
+    bar.start(100, 0);
+    let interval = setInterval(() => {
+      bar.update(torrent.progress * 100);
+    }, 5000);
+    files.forEach((file) => {
+      const source = file.createReadStream();
+      const destination = fs.createWriteStream(file.name);
+      source
+        .on("end", () => {
+          console.log("file : \t\t", file.name);
+          length -= 1;
+          if (!length) {
+            bar.stop();
+            clearInterval(interval);
+            process.exit();
+          }
+        })
+        .pipe(destination);
+    });
+  });
+};
+
+// const parsed = magnetDecode(magnetURI[0].magnet);
 // console.log(parsed.dn); // "Leaves of Grass by Walt Whitman.epub"
 // console.log(parsed.infoHash); // "d2474e86c95b19b8bcfdb92bc12c9d44667cfa36"
 
-//   res.json(magnetURI);
-// });
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
